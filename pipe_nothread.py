b6d24745aa01e890acb4c13a5b08c8fc2bc58455
Bruno Sette <brunosilvasette@gmail.com>
2019-06-22 00:11:29 -0300

    Update

--- pipe_nothread.py
+++ pipe_nothread.py
@@ -73,11 +73,19 @@ def hyperopt_fit_pred(X_train,y_train,X_test,id_test,name,name_dataset):
     print(time)
     preds =  hp.predict(X_test.as_matrix())
     
+    time_df = pd.DataFrame({
+        "name_automl": name,
+        "name_dataset": name_dataset,
+        "time":time
+
+    })
+
     submission = pd.DataFrame({
         "id": id_test,
         "target": preds
     })
-
+    
+    time_df.to_csv(name_dataset+'_'+name+'_submission.csv', index=False)
     submission.to_csv(name_dataset+'_'+name+'_submission.csv', index=False)
     
 
