1d174fa5eef2dbbe1e2f8d6296a4e7cc6d950ac2
Bruno Sette <brunosilvasette@gmail.com>
2019-06-21 23:40:05 -0300

    Update timer

--- benchmark_utils.py
+++ benchmark_utils.py
@@ -7,4 +7,4 @@ def timer(start_time=None):
     elif start_time:
         thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
         tmin, tsec = divmod(temp_sec, 60)
-        return "Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2))"
+        return (thour, tmin, round(tsec, 2))
