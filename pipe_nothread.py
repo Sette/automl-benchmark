e0cb8047b86ad006156855002fe277d17cd687ab
Bruno Sette <brunosilvasette@gmail.com>
2019-06-23 18:03:34 -0300

    Update regressor

--- pipe_nothread.py
+++ pipe_nothread.py
@@ -111,9 +111,9 @@ def hyperopt_fit_pred(X_train,y_train,X_test,id_test,name_dataset):
     
 
 all_models = [
-    ("autosk", autosk_fit_pred),
     ("tpot", tpot_fit_pred),
     ('h2o',h20_fit_pred),
+    ("autosk", autosk_fit_pred),
     ("hyperopt", hyperopt_fit_pred),
 ]
 
