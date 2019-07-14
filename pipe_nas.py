41849529ce1d8fec3e7b566bca003b47df8ec16d
sette <brunosette@protonmail.com>
2019-07-14 17:07:42 -0300

    Load dog dataset

--- pipe_nas.py
+++ pipe_nas.py
@@ -4,15 +4,16 @@
 from autokeras import load_image_dataset
 from autokeras import ImageClassifier
 
+data_dir = datasets/dog-breed/
 
 def load_images():
-    x_train, y_train = load_image_dataset(csv_file_path="train/label.csv",
-                                          images_path="dataset/train")
+    x_train, y_train = load_image_dataset(csv_file_path=data_dir+"/labels.csv",
+                                          images_path=dataset+"/train")
     print(x_train.shape)
     print(y_train.shape)
 
-    x_test, y_test = load_image_dataset(csv_file_path="test/label.csv",
-                                        images_path="test")
+    x_test, y_test = load_image_dataset(csv_file_path=data_dir+"/sample_submission.csv",
+                                        images_path=dataset+"/train")
     print(x_test.shape)
     print(y_test.shape)
     return x_train, y_train, x_test, y_test
@@ -21,11 +22,13 @@ def load_images():
 def run():
     x_train, y_train, x_test, y_test = load_images()
     # After loading train and evaluate classifier.
+    '''
     clf = ImageClassifier(verbose=True, augment=False)
     clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
     y = clf.evaluate(x_test, y_test)
     print(y * 100)
+    '''
 
 
 if __name__ == '__main__':
