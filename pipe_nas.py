250e534b31f4d2a0443d16700d660fb17ddd24a2
sette <brunosette@protonmail.com>
2019-07-15 12:13:06 -0300

    Update nas

--- pipe_nas.py
+++ pipe_nas.py
@@ -1,8 +1,9 @@
 # To run first unzip ../load_raw_image_data.zip into ../
 # so the train and test directories reside in this directory 
 
-from autokeras import load_image_dataset
+
 from autokeras import ImageClassifier
+from autokeras import load_image_dataset
 
 data_dir = datasets/dog-breed/
 
@@ -12,11 +13,14 @@ def load_images():
     print(x_train.shape)
     print(y_train.shape)
 
+    '''
     x_test, y_test = load_image_dataset(csv_file_path=data_dir+"/sample_submission.csv",
                                         images_path=dataset+"/train")
     print(x_test.shape)
     print(y_test.shape)
-    return x_train, y_train, x_test, y_test
+    '''
+    return x_train, y_train
+    
 
 
 def run():
