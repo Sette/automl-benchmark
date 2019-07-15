a985b731aa4e244aed776bb089004c11ca51f872
sette <brunosette@protonmail.com>
2019-07-15 12:30:20 -0300

    Autokeras training

--- pipe_nas.py
+++ pipe_nas.py
@@ -13,26 +13,25 @@ def load_images():
     print(x_train.shape)
     print(y_train.shape)
 
-    '''
-    x_test, y_test = load_image_dataset(csv_file_path=data_dir+"/sample_submission.csv",
-                                        images_path=dataset+"/train")
+    
+    x_test = load_image_dataset(csv_file_path=data_dir+"/sample_submission.csv",
+                                        images_path=data_dir+"/train")
     print(x_test.shape)
-    print(y_test.shape)
-    '''
-    return x_train, y_train
+    
+    return x_train, y_train,x_test
     
 
 
 def run():
-    x_train, y_train, x_test, y_test = load_images()
+    x_train, y_train, x_test = load_images()
     # After loading train and evaluate classifier.
-    '''
+    
     clf = ImageClassifier(verbose=True, augment=False)
     clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
-    clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
-    y = clf.evaluate(x_test, y_test)
-    print(y * 100)
-    '''
+    #clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+    #y = clf.evaluate(x_test, y_test)
+    #print(y * 100)
+    
 
 
 if __name__ == '__main__':
