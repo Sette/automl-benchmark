456432b46ba781f99d842ea7c78053c0d304f003
sette <brunosette@protonmail.com>
2019-07-16 17:18:20 -0300

    Update pipe no thread

--- pipe_nothread.py
+++ pipe_nothread.py
@@ -11,7 +11,7 @@ import pandas as pd
 h2o.init()
 
 all_datasets = [
-        ("microsoft_malware", load_microsoft_malware)
+        ("microsoft_malware", load_microsoft_malware),
         ("porto_seguro", load_porto_seguro),
         ("santander_customer", load_santander_customer),
         
